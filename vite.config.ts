2f89790ea27dfe405d3d5bfb4a2ed5ef925f9fd4
Vllslnt <vllslntng@gmail.com>
2026-07-27 19:37:45 +0700

    Minor tweaking

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/Motor-Vehicle-Insurance-Calculator/",
   plugins: [
     react(),
     tailwindcss(),
